63a2727eee4f98060bbcd95734f591ce43a65712
Sharayu Yadav <ysharayu18@gmail.com>
2023-04-28 21:42:58 +0530

    Add files via upload

---
+++ Javascript/LearnCodeOnline/02/04_hoisting_lyst2895.js
@@ -0,0 +1,31 @@
+tipper("80");
+
+function tipper(a) {
+  var bill = parseInt(a);
+  console.log(bill + 5);
+}
+
+var bigTipper = function (a) {
+  var bill = parseInt(a);
+  console.log(bill + 15);
+};
+
+bigTipper("200");
+
+console.log(name); //undefined
+var name = "hitesh";
+
+function sayName() {
+  var name = "MR. H";
+  console.log(name);
+}
+sayName();//MR. H
+
+console.log(name); //hitesh
+
+// VVIMP thing about hoisting 
+
+//  -> global contexts scans and makes "functions" -> "available"
+//  -> global contexts scans and makes "variables" -> "undefined"
+
+ // This is the eternal truth be it any situationS
