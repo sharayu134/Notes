e13004ba7c5bef1cf5c4fd65f2c9fda4d8c1cac3
Sharayu Yadav <ysharayu18@gmail.com>
2023-04-28 20:08:12 +0530

    Update truthyAndFalsy_coercion.js

--- Javascript/LearnCodeOnline/02/truthyAndFalsy_coercion.js
+++ Javascript/LearnCodeOnline/02/truthyAndFalsy_coercion.js
@@ -2,7 +2,7 @@ undefined   {variable which is notgiven a value but defined var myVariable; - an
 null     { is when we are supposed to get the value from somewhere but we did not receive it, like fx rate came null from service // intentional absence of any object value }  
 0       { zero is zero number zero}
 ''      { empty string }
-None     { https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN#:~:text=NaN%20is%20a%20property%20of,the%20case%2C%20avoid%20overriding%20it.
+NAN    { https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN#:~:text=NaN%20is%20a%20property%20of,the%20case%2C%20avoid%20overriding%20it.
         Failed number conversion (e.g. explicit ones like parseInt("blabla"), Number(undefined), or implicit ones like Math.abs(undefined))}
 
 var user = "2";
