f403312eef06c2c5ccfce300221a6d3344e86c13
Sharayu Yadav <ysharayu18@gmail.com>
2023-04-28 19:58:04 +0530

    Add files via upload

---
+++ Javascript/LearnCodeOnline/02/09_TruthyAndFalsy_lyst1171.js
@@ -0,0 +1,18 @@
+//TODO: falsy
+// undefined
+// null
+// 0
+// ''
+// NaN
+
+var user = "2";
+
+if (2 == user) {
+  console.log("Condition is true");
+} //prints it and its called cohersion
+
+if (2 == user) {
+  console.log("Condition is true");
+} //it will not print the statement this is why we use ===
+
+// console.log("2" + "2");
