bb984a317c90a95cc26c9555597a130388dbae16
Sharayu Yadav <ysharayu18@gmail.com>
2023-04-29 11:35:51 +0530

    Add files via upload

---
+++ Javascript/LearnCodeOnline/02/06_thisKeyword_lyst7635.js
@@ -0,0 +1,14 @@
+//TODO: part 1 for this keyword
+
+console.log(this); // this tries to access the scope // output is {} if we run with node 
+
+//if we do same thing in browser it will print the window which is scope for browser js engine
+
+var game = "basketball";
+
+// function sayName() {
+//   var name = "Hitesh";
+//   console.log(this);
+// }
+
+// sayName();
