9dcaca05f89aaf6b011a458dff23726c52b65b70
Sharayu Yadav <ysharayu18@gmail.com>
2023-04-29 12:18:19 +0530

    Add files via upload

---
+++ Javascript/LearnCodeOnline/0304/09_filterArray_lyst7966.js
@@ -0,0 +1,20 @@
+var testArray = [2, 4, 6, 3, 1, 5, 9, 8];
+
+//console.log(testArray.fill("empty")); will fill whole array with "empty")
+// testArray.fill("h", 2, 5) -> [2, 4, "h", "h", "h", 5, 9, 8]
+// 2 is start  [inclusive indexes start with 0]
+// 5 is end     [exclusive indexes start with 0]
+const myNumber = [23, 24, 25, 55, 66, 77, 87, 98];
+
+const result = myNumber.filter((num) => num > 55);
+
+//console.log(result);
+
+// Yes, Same exercise file
+
+var users = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"];
+
+// console.log(users.slice(1));
+users.splice(1, 3, "HI", "BYE");
+// users.splice()
+console.log(users);
