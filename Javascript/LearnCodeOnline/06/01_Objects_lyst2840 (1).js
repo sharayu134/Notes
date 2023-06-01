6404a5669880f47f224eaaaea8e4067a004ed78d
Sharayu Yadav <ysharayu18@gmail.com>
2023-06-01 19:02:27 +0530

    Update 01_Objects_lyst2840 (1).js

--- Javascript/LearnCodeOnline/06/01_Objects_lyst2840 (1).js
+++ Javascript/LearnCodeOnline/06/01_Objects_lyst2840 (1).js
@@ -23,3 +23,21 @@ var sam = new User("Sam", 1);
 sam.getCourseCount();
 sam.getFirstname();
 //console.log(sam);
+
+// Prototyping 
+// is related to objects
+// object in js is like 
+User  = {
+  name : "sharayu",
+  lastname : "yadav"
+}
+// prototyping can make it reusable
+var user = function(name, lName){
+  name: this.name,
+  lName: this.lName
+}
+prasad = new user("prasad", "marne")
+// you can add getter and setter as well
+User.prototype.getFirstname = function () { //like getter and setter 
+  console.log(`Your firstname is : ${this.firstName}`);
+};
