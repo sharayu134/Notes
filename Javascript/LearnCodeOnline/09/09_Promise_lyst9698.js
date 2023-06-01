2c9a6bfc559ba6641fb286c524689a3037f2d327
Sharayu Yadav <ysharayu18@gmail.com>
2023-06-01 20:57:59 +0530

    Update 09_Promise_lyst9698.js

--- Javascript/LearnCodeOnline/09/09_Promise_lyst9698.js
+++ Javascript/LearnCodeOnline/09/09_Promise_lyst9698.js
@@ -36,3 +36,67 @@ callMe();
 // I am One
 // I am Two
 // I am Three
+
+// Promise is to be resolved or rejected 
+const p = new Promise((resolve, reject )=>{
+    a = 1+3;
+    if(a===2){
+        resolve("message")
+    }else{
+        reject("erro")
+    }
+})
+
+// once resolved or rejected use .then() and catch()
+p.then((msg)=>{
+    console.log(msg)
+}).catch((msg)=>{
+    console.log(msg)
+})
+
+// Process a bunch or array
+const p = new Promise((resolve, reject )=>{
+    a = 1+1;
+    if(a===2){
+        resolve("message")
+    }else{
+        reject("erro")
+    }
+})
+const p1 = new Promise((resolve, reject )=>{
+    a = 1+1;
+    if(a===2){
+        resolve("message1")
+    }else{
+        reject("erro")
+    }
+})
+const p2 = new Promise((resolve, reject )=>{
+    a = 1+1;
+    if(a===2){
+        resolve("message2")
+    }else{
+        reject("erro")
+    }
+})
+
+Promise.all(
+    [p,p1,p2]
+).then((msg)=>{ console.log(msg)}   )
+
+// output
+D:\LEARNING\javascript\HTMLCSSJSBASIC Layout>node ext.js
+message
+[ 'message', 'message1', 'message2' ]
+
+D:\LEARNING\javascript\HTMLCSSJSBASIC Layout>
+  
+  
+Promise.race(
+    [p,p1,p2]
+).then((msg)=>{ console.log(msg)}   )
+// output
+D:\LEARNING\javascript\HTMLCSSJSBASIC Layout>node ext.js
+message
+
+D:\LEARNING\javascript\HTMLCSSJSBASIC Layout>
