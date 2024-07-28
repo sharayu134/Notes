664f805d56eb38cd17429b4e9b0062958916c209
Sharayu Yadav <ysharayu18@gmail.com>
2024-07-28 11:44:14 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,6 @@
 # https://www.youtube.com/watch?v=0K_eZGS5NsU&ab_channel=NeetCode
 # https://www.youtube.com/watch?v=_uQrJ0TkZlc&ab_channel=ProgrammingwithMosh
+
+IMP
+# https://www.dataquest.io/blog/python-copy-list/
+# https://developers.google.com/edu/python/sorting   sorting with key
