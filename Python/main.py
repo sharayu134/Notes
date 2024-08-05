b58b5ed8e244cb8520378f4ee0cd1e79bb11bed9
Sharayu Yadav <ysharayu18@gmail.com>
2024-08-05 22:18:06 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,10 +5,13 @@ IMP
 # https://www.dataquest.io/blog/python-copy-list/
 # https://developers.google.com/edu/python/sorting   sorting with key
 
-```
+# String to array split using list
 s = 'cerfe'
 s = list(s)
 # s = [c, e, r, f, e]
+
+# Join list to array using join
 ''.join(s)
 # s = 'cerfe'
+
 'abcd'.startswith('ab'):  true
