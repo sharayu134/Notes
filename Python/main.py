63981e9ff623388fde0d8313ad6ad8d176eea0b4
Sharayu Yadav <ysharayu18@gmail.com>
2024-08-05 22:16:54 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,4 +5,10 @@ IMP
 # https://www.dataquest.io/blog/python-copy-list/
 # https://developers.google.com/edu/python/sorting   sorting with key
 
+```
+s = 'cerfe'
+s = list(s)
+# s = [c, e, r, f, e]
+''.join(s)
+# s = 'cerfe'
 'abcd'.startswith('ab'):  true
